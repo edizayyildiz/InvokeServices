52b79eea513e8e73b2ee82172f204dc72f6a6227
edizayyildiz <ayyildizediz@gmail.com>
2019-03-01 02:47:22 +0300

    WebApplication1 projesinden Service çağırdığım çalışma (Hem WCF Hem XML WebService kullanarak)

---
+++ InvokeServices/CallService.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="Call Services" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CallService.aspx.cs" Inherits="InvokeServices.CallService" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <p>Call Some Services.</p>
+    <div class="form-group">
+        <label for="formGroupExampleInput">WCF(Windows Communication Foundation)</label>
+        <asp:TextBox ID="txtWCFId" class="form-control" runat="server" placeholder="WCF bir SOA framework'üdür" />
+        <asp:TextBox ID="txtWCFTitle" class="form-control" runat="server" />
+        <asp:TextBox ID="txtWCFDescription" class="form-control" runat="server" />
+    </div>
+    <div class="form-group">
+        <label for="formGroupExampleInput2">XML Service</label>
+        <asp:TextBox ID="txtXMLId" class="form-control" runat="server" />
+        <asp:TextBox ID="txtXMLTitle" class="form-control" runat="server" />
+        <asp:TextBox ID="txtXMLDescription" class="form-control" runat="server" />
+    </div>
+    <div class="btn-group">
+        <asp:Button ID="Button1" CssClass="btn btn-primary" runat="server" Text="WCF" OnClick="Button1_Click" />
+        <asp:Button ID="Button2" CssClass="btn btn-primary" runat="server" Text="XML" OnClick="Button2_Click" />
+    </div>
+</asp:Content>
---
+++ InvokeServices/CallService.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InvokeServices
+{
+    public partial class CallService : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtWCFId.Text);
+            PostServiceReference.PostServiceClient postWcfService = new PostServiceReference.PostServiceClient();
+            if (id == 1)
+            {
+                var post = postWcfService.GetPosts().FirstOrDefault(p=>p.Id==id);
+                txtWCFTitle.Text = post.Title;
+                txtWCFDescription.Text = post.Description;
+            }
+            else if (id == 2)
+            {
+                var post= postWcfService.GetPosts().FirstOrDefault(p => p.Id == id);
+                txtWCFTitle.Text = post.Title;
+                txtWCFDescription.Text = post.Description;
+            }
+            else
+            {
+                txtWCFTitle.Text = "Yanlış değer girdiniz";
+                txtWCFDescription.Text = "Yanlış değer girdiniz";
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtXMLId.Text);
+            PostXMLServiceReference.PostXmlServiceSoapClient postXmlService = new PostXMLServiceReference.PostXmlServiceSoapClient();
+            if (id == 1)
+            {
+                var postArray = postXmlService.GetPosts();
+                txtXMLTitle.Text = postArray[0].Title;
+                txtXMLDescription.Text = postArray[0].Description;
+            }
+            else if (id == 2)
+            {
+                var postArray = postXmlService.GetPosts();
+                txtXMLTitle.Text = postArray[1].Title;
+                txtXMLDescription.Text = postArray[1].Description;
+            }
+            else
+            {
+                txtXMLTitle.Text = "Yanlış değer girdiniz";
+                txtXMLDescription.Text = "Yanlış değer girdiniz";
+            }
+
+        }
+    }
+}
